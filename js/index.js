02c6235e44fbc5cab8e4ec6d77c165563e373395
Edilson-EJ <edilsonjunior624@gmail.com>
2022-08-01 22:22:46 -0300

    adicionando funcionalidade de contagem

--- js/index.js
+++ js/index.js
@@ -1,28 +1,78 @@
-var minutosHT = document.getElementById('minutos');
-let segundosHT = document.getElementById('segundos');
+var time = document.getElementById('time');
 var buttonHT = document.getElementById('button');
 
 var pomodoro = 25;
 var pausa = 5;
 var pausa_grande = 15;
 var segun = 0;
+var min = 0
+var sec = 0
+var interval
 
-console.log(pomodoro)
+time.textContent = pomodoro + ':00'
 
-time();
-
-minutosHT.textContent = pomodoro
-segundosHT.textContent = segun + '0'
-
-function time(){
-        var minutos = 25
-        var segundos
-        
-        for(i=0; minutosHT != 0; i++){
-                minutos = Math.floor(diferença / 1000 /60) % 60 // minutos
-                segundos = Math.floor(diferença / 1000) % 60
-        }
-
-
+function inicio(){
+        console.log('Inicio')
+        interval = setInterval(tempo_estudo,10)
 }
 
+function intervalo_curto(){
+        console.log('intervalo curto')
+        interval = setInterval(pausa_curta,10)
+}
+
+function intervalo_grande(){
+        console.log('Intervalo Grande')
+        interval = setInterval(pausa_longa,10)
+}
+
+function tempo_estudo(){
+        sec ++
+        if(sec== 60){
+                min++
+                sec=0
+                if( min == 10){
+                        window.alert('fim!!')
+                        interval = setInterval(intervalo_curto,10)
+                }
+        }
+                
+        
+        document.getElementById('time').innerText = min + ':' + sec
+}
+
+function pausa_curta() {
+
+        sec ++
+        if(sec == 60){
+                console.log('entrouuuuuuuuuuuu')
+                min++
+                console.log(min)
+                sec=0
+                if( min == 5){
+                        window.alert('terminou a folga!!')
+                        inicio();
+                }
+        }
+                
+        
+        document.getElementById('time').innerText = min + ':' + sec
+}
+
+function pausa_longa() {
+
+        sec ++
+        if(sec == 60){
+                console.log('pausa bem longaaaaaaaaa')
+                min++
+                console.log(min)
+                sec=0
+                if( min == 15){
+                        window.alert('terminou a folga!!')
+                        inicio();
+                }
+        }
+                
+        
+        document.getElementById('time').innerText = min + ':' + sec
+}
