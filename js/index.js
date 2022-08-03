2554742b7d6d734c838fef907d27f0810285fa93
Edilson-EJ <edilsonjunior624@gmail.com>
2022-08-02 22:11:41 -0300

    time add

--- js/index.js
+++ js/index.js
@@ -8,71 +8,111 @@ var segun = 0;
 var min = 0
 var sec = 0
 var interval
+var pomo = 0
 
 time.textContent = pomodoro + ':00'
 
 function inicio(){
-        console.log('Inicio')
-        interval = setInterval(tempo_estudo,10)
+        usuario(1);
 }
 
 function intervalo_curto(){
-        console.log('intervalo curto')
-        interval = setInterval(pausa_curta,10)
+        usuario(2);
 }
 
 function intervalo_grande(){
-        console.log('Intervalo Grande')
-        interval = setInterval(pausa_longa,10)
+        usuario(3);
+}
+
+function usuario(number){
+        if(number == 1){
+                interval = setInterval(tempo_estudo,10)
+                console.log('1')
+
+        }else if (number == 2){
+                interval = setInterval(pausa_curta,10)
+                console.log('2')
+        }else if(number == 3) {
+                interval = setInterval(pausa_longa,10)
+                console.log('3')
+        }else{
+                number=0;
+                window.alert('parabéns você esta no seu ' + pomo + ' pomodoro!!')
+                zero();
+        }
 }
 
 function tempo_estudo(){
-        sec ++
+        console.log('tempo de estudo')
+        sec++
+                
         if(sec== 60){
                 min++
                 sec=0
                 if( min == 10){
                         window.alert('fim!!')
-                        interval = setInterval(intervalo_curto,10)
+                        usuario(0);
+                        pomodoro = 2;
                 }
         }
+                        
                 
-        
-        document.getElementById('time').innerText = min + ':' + sec
+                document.getElementById('time').innerText = min + ':' + sec
 }
 
-function pausa_curta() {
-
-        sec ++
-        if(sec == 60){
-                console.log('entrouuuuuuuuuuuu')
+function pausa_curta(){
+        console.log('pausa curta')
+        sec++
+                
+        if(sec== 60){
                 min++
-                console.log(min)
                 sec=0
                 if( min == 5){
-                        window.alert('terminou a folga!!')
-                        inicio();
+                        window.alert('fim!!')
+                        usuario(0);
+                        
                 }
         }
+                        
                 
-        
-        document.getElementById('time').innerText = min + ':' + sec
+                document.getElementById('time').innerText = min + ':' + sec
 }
 
-function pausa_longa() {
-
-        sec ++
-        if(sec == 60){
-                console.log('pausa bem longaaaaaaaaa')
+function pausa_longa(){
+        console.log('pausa longa')
+        sec++
+                
+        if(sec== 60){
                 min++
-                console.log(min)
                 sec=0
                 if( min == 15){
-                        window.alert('terminou a folga!!')
-                        inicio();
+                        window.alert('fim!!')
+                        usuario(0);
+                        
                 }
         }
+                        
                 
-        
-        document.getElementById('time').innerText = min + ':' + sec
+                document.getElementById('time').innerText = min + ':' + sec
 }
+
+function zero(){
+        clearInterval(interval)
+        console.log('zero')
+        console.log('pomodoro: ',pomo)
+        sec = 0
+        min = 0
+        
+        if(pomo == 1 || pomo == 3 || pomo == 5){
+                inicio();
+                pomo ++
+        }else if( pomo == 0 || pomo == 2 || pomo == 4){
+                intervalo_curto();
+                pomo ++
+        }else if (pomo == 6){
+                intervalo_grande();
+                pomo = 1;
+        }
+
+}
+
