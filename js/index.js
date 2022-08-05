46a3fe5705360bf0f142afb3c5bdf0371ae203db
Edilson-EJ <edilsonjunior624@gmail.com>
2022-08-04 22:40:56 -0300

    adicionando funcionalidades

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 var time = document.getElementById('time');
-var buttonHT = document.getElementById('button');
+var button = document.getElementById('button');
+var container = document.querySelector('.container')
+var body = document.querySelector('body')
+var foco = document.querySelector('.quant_pom');
+var prog = document.getElementById('tempo')
+var tarefa = document.querySelector('.tarefas')
+var progresso_pessoa = document.querySelector('.progresso')
+
 
 var pomodoro = 25;
 var pausa = 5;
@@ -8,20 +15,58 @@ var segun = 0;
 var min = 0
 var sec = 0
 var interval
-var pomo = 0
+var pomo = 1
+var cont_pausas = 1
+var bool = false
+
 
 time.textContent = pomodoro + ':00'
 
-function inicio(){
+function inicio(){   
+        button.innerText = 'para';
+        container.style.backgroundColor = "#f26868";
+        body.style.backgroundColor = "#d95550";
+        foco.style.backgroundColor =  "#f26868";
+        tarefa.style.backgroundColor =  "#f26868";
+        progresso_pessoa.style.backgroundColor = "#f26868";
+        progresso();
+        bool= true
         usuario(1);
+        
 }
 
 function intervalo_curto(){
-        usuario(2);
+
+        container.style.backgroundColor = "#768de6"
+        body.style.backgroundColor = "#556ed3";
+        foco.style.backgroundColor = "#768de6"
+        tarefa.style.backgroundColor =  "#768de6";
+        progresso_pessoa.style.backgroundColor = "#768de6";
+
+        time.textContent = '05:00'
+        progresso();
+        if(bool == true){
+                button.innerText = 'para'
+                usuario(2);
+        }
+        
+        
 }
 
 function intervalo_grande(){
-        usuario(3);
+
+        container.style.backgroundColor = "#5e9ca0";
+        body.style.backgroundColor =  "#4c9195";
+        foco.style.backgroundColor = "#5e9ca0";
+        tarefa.style.backgroundColor =  "#5e9ca0";
+        progresso_pessoa.style.backgroundColor = "#5e9ca0";
+
+        progresso();
+        if(bool == true){
+                button.innerText = 'para'
+                usuario(3);
+        }
+        
 }
 
 function usuario(number){
@@ -37,6 +82,9 @@ function usuario(number){
                 console.log('3')
         }else{
                 number=0;
+                pomo++
+                total_foco.innerText = '#' + pomo
+                button.innerText = "Começa"
                 window.alert('parabéns você esta no seu ' + pomo + ' pomodoro!!')
                 zero();
         }
@@ -52,12 +100,13 @@ function tempo_estudo(){
                 if( min == 10){
                         window.alert('fim!!')
                         usuario(0);
-                        pomodoro = 2;
+                        
                 }
         }
                         
                 
                 document.getElementById('time').innerText = min + ':' + sec
+                
 }
 
 function pausa_curta(){
@@ -100,19 +149,38 @@ function zero(){
         clearInterval(interval)
         console.log('zero')
         console.log('pomodoro: ',pomo)
+
         sec = 0
         min = 0
-        
-        if(pomo == 1 || pomo == 3 || pomo == 5){
+
+        console.log('contador de pausas: ', cont_pausas)
+
+        if(cont_pausas  %2 == 0 && cont_pausas  != 5){
                 inicio();
-                pomo ++
-        }else if( pomo == 0 || pomo == 2 || pomo == 4){
+                cont_pausas++
+
+        }else if( cont_pausas  %2 != 0 && cont_pausas  != 5){
                 intervalo_curto();
-                pomo ++
-        }else if (pomo == 6){
+                cont_pausas++
+
+        }else if (cont_pausas == 5 ){
                 intervalo_grande();
-                pomo = 1;
+                cont_pausas = 0
         }
 
+}
+
+function progresso(){
+        var divNova = document.createElement('div');
+        var conteudo = document.createElement('p');
+        var divPro = document.getElementById('tempo');
+
+        const horario = new Date().toLocaleTimeString(); 
+
+        conteudo.textContent = pomo + ' - '+ horario;
+
+        divNova.appendChild(conteudo);
+        divPro.appendChild(divNova);
+        
 }
 
